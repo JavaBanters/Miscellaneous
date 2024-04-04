01741a3950253c4c0f84ef318873512d7e02639b
Anant Majhi <anmajhi7@gmail.com>
2024-04-04 09:18:19 +0530

    reversal of a number

--- src/recursion/ReverseNumber.java
+++ src/recursion/ReverseNumber.java
@@ -5,16 +5,28 @@ public class ReverseNumber {
     static int result = 0;
     public static void main(String[] args) {
         int num = 12345;
-        //reverse(num);
-        reverseRecursion(num);
+        //reverse(num); // via iterative
+        //reverseRecursion(num); // via recursion
+        int result = reverseRecursionSimplyfied(num);
         System.out.println(result);
     }
+
+    private static int reverseRecursionSimplyfied(int num) {
+        int digits = String.valueOf(num).length();
+        return helper(num, digits);
+    }
+
+    private static int helper(int num, int digits) {
+        if(num%10 == num) return num;
+        return (int) (num%10 * Math.pow(10, digits - 1) + helper(num/10, digits-1));
+    }
+
 
     private static void reverseRecursion(int num) {
         if(num == 0) return;
         int rem = num % 10;
         result = result * 10 + rem;
-        reverseRecursion(num/10); // 12345  1234 123 12 1 0
+        reverseRecursion(num/10); //12345  1234 123 12 1 0
     }
 
     public static void reverse(int num) {
